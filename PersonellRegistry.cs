36f5055f2cea6d3c33983a24bfc5f1d6ccf8d019
Tomas <tomasantonj@gmail.com>
2025-05-21 16:34:39 +0200

    Added comments and TODOs

--- PersonellRegistry.cs
+++ PersonellRegistry.cs
@@ -18,12 +18,12 @@ namespace Lexicon_personalregister
             this.name = name;
             this.wage = wage;
         }
-        // method to add a new PersonellRegistry object to the list
+        // constructor to add a new PersonellRegistry object to the list
         public void AddPersonellRegistry(string name, int wage)
         {
 
         }
-        // overloaded method that appends newly created PersonellRegistry objects to the list and prompts user for more input and displays the list and ends the program
+        // overloaded constructor method that appends newly created PersonellRegistry objects to the list and prompts user for more input and displays the list and ends the program
         // TODO break this method into smaller methods for better readability and maintainability
         public void AddPersonellRegistry()
         {
@@ -33,6 +33,7 @@ namespace Lexicon_personalregister
             // prompt user for input and run some checks on the input data to make sure it is valid
             Console.WriteLine("Enter name:");
             string name = Console.ReadLine();
+            // TODO: Move all input validation to a separate method for better readability and maintainability
             // check if name is empty or null
             while (string.IsNullOrEmpty(name))
             {
@@ -41,12 +42,14 @@ namespace Lexicon_personalregister
             }
 
             Console.WriteLine("Enter wage:");
-            int wage = int.Parse(Console.ReadLine());
-            // check if wage is an integer and a number greater than 0
-            while (wage < 0)
+            // ask user for input for wage, and run check to verify it is a number
+            string wageInput = Console.ReadLine();
+            int wage;
+            // check if wage is a number and run some checks on the input data to make sure it is valid
+            while (!int.TryParse(wageInput, out wage) || wage < 0)
             {
-                Console.WriteLine("Wage must be a number greater than 0. Please enter a valid wage:");
-                wage = int.Parse(Console.ReadLine());
+                Console.WriteLine("Wage must be a positive number. Please enter a valid wage:");
+                wageInput = Console.ReadLine();
             }
 
             // create a new PersonellRegistry object and add it to the list
@@ -56,7 +59,13 @@ namespace Lexicon_personalregister
             // ask if user wants to add more registries
             Console.WriteLine("Do you want to add more registries? (yes/no)");
             string answer = Console.ReadLine();
-            //
+            // check if answer is yes or no and run some checks on the input data to make sure it is valid
+            while (answer.ToLower() != "yes" && answer.ToLower() != "no")
+            {
+                Console.WriteLine("Please enter a valid answer (yes/no):");
+                answer = Console.ReadLine();
+            }
+            // TODO: Move logic to a separate method for better readability and maintainability
             while (answer.ToLower() == "yes")
             {
                 Console.WriteLine("Enter name:");
@@ -66,6 +75,12 @@ namespace Lexicon_personalregister
                 personellRegistry = new PersonellRegistry(name, wage);
                 personellRegistries.Add(personellRegistry);
                 Console.WriteLine("Do you want to add more registries? (yes/no)");
+                // check if answer is yes or no and run some checks on the input data to make sure it is valid
+                while (answer.ToLower() != "yes" && answer.ToLower() != "no")
+                {
+                    Console.WriteLine("Please enter a valid answer (yes/no):");
+                    answer = Console.ReadLine();
+                }
                 answer = Console.ReadLine();
             } if (answer.ToLower() == "no")
             {
@@ -78,9 +93,10 @@ namespace Lexicon_personalregister
             }
         }
         // method that displays all PersonellRegistry objects in the list
+        // TODO figure out why the Console.WriteLine Personell Registries is outputted twice at end of program
         public void DisplayPersonellRegistries(List<PersonellRegistry> personellRegistries)
         {
-            Console.WriteLine("Personell Registries222:");
+            Console.WriteLine("Personell Registries:");
             foreach (var personellRegistry in personellRegistries)
             {
                 Console.WriteLine($"Name: {personellRegistry.name}, Wage: {personellRegistry.wage}");
