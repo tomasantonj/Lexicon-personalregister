0e203b870b7056bd20ac15f78786f0b84f86e789
Tomas <tomasantonj@gmail.com>
2025-05-21 15:36:16 +0200

    Moved most functionality into the personellregistry class Still need to adress issues with program flow and handling of input/output data

--- PersonellRegistry.cs
+++ PersonellRegistry.cs
@@ -18,5 +18,51 @@ namespace Lexicon_personalregister
             this.name = name;
             this.wage = wage;
         }
+        // method to add a new PersonellRegistry object to the list
+        public void AddPersonellRegistry(string name, int wage)
+        {
+            PersonellRegistry personellRegistry = new PersonellRegistry(name, wage);
+            List<PersonellRegistry> personellRegistries = new List<PersonellRegistry>();
+            personellRegistries.Add(personellRegistry);
+        }
+        // method that creates a new PersonellRegistry object and adds it to the list
+        public void AddPersonellRegistry()
+        {
+            Console.WriteLine("Enter name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter wage:");
+            int wage = int.Parse(Console.ReadLine());
+            PersonellRegistry personellRegistry = new PersonellRegistry(name, wage);
+            List<PersonellRegistry> personellRegistries = new List<PersonellRegistry>();
+            personellRegistries.Add(personellRegistry);
+
+            // display the added PersonellRegistry object
+            //Console.WriteLine($"Added PersonellRegistry: Name: {personellRegistry.name}, Wage: {personellRegistry.wage}");
+        }
+
+        // method that displays all the personell registries in the list and asks user if he wants to add additional users
+        public void DisplayPersonellRegistries(List<PersonellRegistry> personellRegistries)
+        {
+            Console.WriteLine("Personell Registries:");
+            foreach (var personellRegistry in personellRegistries)
+            {
+                Console.WriteLine($"Name: {personellRegistry.name}, Wage: {personellRegistry.wage}");
+            }
+            // ask if user wants to add more registries and while the user says yes keep adding registries
+            Console.WriteLine("Do you want to add more registries? (yes/no)");
+            string answer = Console.ReadLine().ToLower();
+            while (answer == "yes")
+            {
+                AddPersonellRegistry();
+                Console.WriteLine("Do you want to add more registries? (yes/no)");
+                answer = Console.ReadLine().ToLower();
+            }
+            // display the final list of registries
+            Console.WriteLine("Curren list looks like this:");
+            foreach (var personellRegistry in personellRegistries)
+            {
+                Console.WriteLine($"Name: {personellRegistry.name}, Wage: {personellRegistry.wage}");
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,60 +5,14 @@ namespace Lexicon_personalregister
     {
         static void Main(string[] args)
         {
-            CreatePersonellRegistry();
+            // Create an instance of PersonellRegistry
+            PersonellRegistry personellRegistry = new PersonellRegistry("", 0);
+            // Call the method to add a new PersonellRegistry object to the list
+            personellRegistry.AddPersonellRegistry();
+            // Call the method to display all PersonellRegistry objects in the list
+            personellRegistry.DisplayPersonellRegistries(new List<PersonellRegistry>());
         }
-        // method to create a new PersonellRegistry object using command line input
-        static PersonellRegistry CreatePersonellRegistry()
-        {
-            Console.WriteLine("Enter name 11:");
-            string name = Console.ReadLine();
-            Console.WriteLine("Enter wage 11:");
-            int wage = int.Parse(Console.ReadLine());
+        // method that collects information
 
-            // Display the newly created PersonellRegistry object
-            Console.WriteLine($"Name: {name}, Wage: {wage}");
-
-            // Parse input data into a collection of PersonellRegistry objects
-            List<PersonellRegistry> personellRegistries = new List<PersonellRegistry>();
-            PersonellRegistry personellRegistry = new PersonellRegistry(name, wage);
-            personellRegistries.Add(personellRegistry);
-
-            // Add the new PersonalRegistry to the list with method AddPersonellRegistry and return it
-            return AddPersonellRegistry(personellRegistries)
-        }
-
-        // Method to handle the list object from CreatePersonellRegistry and add it to the list
-        static PersonellRegistry AddPersonellRegistry(List<PersonellRegistry> personellRegistries)
-        {
-            Console.WriteLine("Do you want to add another PersonellRegistry? (yes/no)");
-            string answer = Console.ReadLine().ToLower();
-            while (answer == "yes")
-            {
-                Console.WriteLine("Enter name 22:");
-                string name = Console.ReadLine();
-                Console.WriteLine("Enter wage 22:");
-                int wage = int.Parse(Console.ReadLine());
-                // Create a new PersonellRegistry object and add it to the list
-                PersonellRegistry personellRegistry = new PersonellRegistry(name, wage);
-                personellRegistries.Add(personellRegistry);
-                // Ask if the user wants to add another one
-                Console.WriteLine("Do you want to add another PersonellRegistry? (yes/no)");
-                answer = Console.ReadLine().ToLower();
-            }
-            // Display all PersonellRegistry objects in the list
-            DisplayPersonellRegistries(personellRegistries);
-            return personellRegistries[0]; // Return the first object in the list as an example
-        }
-
-
-        // method to display all PersonellRegistry objects in a list
-        static void DisplayPersonellRegistries(List<PersonellRegistry> personellRegistries)
-        {
-            Console.WriteLine("Personell Registries 333:");
-            foreach (var personellRegistry in personellRegistries)
-            {
-                Console.WriteLine($"Name: {personellRegistry.name}, Wage: {personellRegistry.wage}");
-            }
-        }
     }
 }
